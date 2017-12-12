8461409d8e14c306e34ef5748ea4aa742b9c0bc2
Caroline Hirwing <caroline@hirwing.se>
2017-12-12 19:24:04 +0100

    Split up some stuff to a common module

--- hosts/huang/configuration.nix
+++ hosts/huang/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../profiles/common.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -15,19 +16,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "huang";
-
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "dvorak-sv-a1";
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [
-      "all"
-    ];
-  };
-
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -58,27 +46,7 @@
     xkb-options=['eurosign:e', 'ctrl:nocaps', 'numpad:mac', 'kpdl:dot']
   '';
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
   networking.networkmanager.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -100,19 +68,4 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.autoLogin.enable = true;
   services.xserver.displayManager.gdm.autoLogin.user = "concate";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.concate = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = [ "wheel" ];
-    initialPassword = "a";
-  };
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
-
 }
---
+++ profiles/common.nix
@@ -0,0 +1,68 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09";
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "dvorak-sv-a1";
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "all"
+    ];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+  services.openssh.passwordAuthentication = false;
+
+  # Enable fish
+  programs.fish.enable = true;
+  programs.mosh.enable = true;
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    ag
+    ccid              # Used for smartcards
+    curl
+    dnsutils
+    emacs
+    file
+    fzf
+    git
+    gnupg
+    htop
+    host
+    ncdu
+    nfs-utils
+    oathToolkit       # Used by the pass otp etxension
+    pass
+    pv
+    stow
+    sshfs-fuse
+    tmux
+    youtube-dl
+    whois
+  ];
+
+  # Enable firewall.
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+
+  # Define a user account.
+  users.extraUsers.concate.isNormalUser = true;
+  users.extraUsers.concate.uid = 1000;
+  users.extraUsers.concate.extraGroups = [ "wheel" ];
+}
